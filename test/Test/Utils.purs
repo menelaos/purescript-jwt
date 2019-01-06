286a5a99b9196daee2b57c833a86f4e30c8b628a
Andreas Schacker <menelaos@users.noreply.github.com>
2019-01-06 23:16:15 +0100

    Update for PureScript 0.12.1

--- test/Test/Utils.purs
+++ test/Test/Utils.purs
@@ -4,7 +4,7 @@ where
 
 import Control.Error.Util   (note)
 import Data.Argonaut.Core   (Json, toObject)
-import Data.Argonaut.Decode ((.?))
+import Data.Argonaut.Decode ((.:))
 import Data.Either          (Either)
 import Prelude
 
@@ -12,4 +12,4 @@ import Prelude
 getUsername :: Json -> Either String String
 getUsername json = do
   obj <- note "Could not parse JSON as object" $ toObject json
-  obj .? "username"
+  obj .: "username"

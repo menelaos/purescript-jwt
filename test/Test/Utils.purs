e03b7398f65a607cd2765aeae4ff42400f8f784e
Carsten König <Carsten.Koenig@hotmail.de>
2021-03-03 07:20:55 +0100

    update to purescript 0.14

--- test/Test/Utils.purs
+++ test/Test/Utils.purs
@@ -2,14 +2,15 @@ module Test.Utils
   ( getUsername )
 where
 
-import Control.Error.Util   ( note )
-import Data.Argonaut.Core   ( Json, toObject )
-import Data.Argonaut.Decode ( (.:) )
-import Data.Either          ( Either )
 import Prelude
 
+import Control.Error.Util (note)
+import Data.Argonaut.Core (Json, toObject)
+import Data.Argonaut.Decode (JsonDecodeError(..), (.:))
+import Data.Either (Either)
 
-getUsername :: Json -> Either String String
+
+getUsername :: Json -> Either JsonDecodeError String
 getUsername json = do
-  obj <- note "Could not parse JSON as object" $ toObject json
+  obj <- note (TypeMismatch "Could not parse JSON as object") $ toObject json
   obj .: "username"
